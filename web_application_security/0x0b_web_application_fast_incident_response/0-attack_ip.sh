1ad02ff4a926aeebf90c53364cf1b8b1178d5aeb
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-04-01 08:31:01 -0400

    fix: sort IP counts numerically and print only the top IP

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' $1 | sort | uniq -c | sort -r | head -1
+grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' $1 | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
