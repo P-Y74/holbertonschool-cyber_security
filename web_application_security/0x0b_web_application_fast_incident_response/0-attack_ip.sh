93f54799af1aab3827e8d47a4adfa9df59ddd220
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-04-01 08:37:43 -0400

    fix: use logs.txt as default input file

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' $1 | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
