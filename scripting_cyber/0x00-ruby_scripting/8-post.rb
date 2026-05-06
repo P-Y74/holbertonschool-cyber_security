1c54192a49539ad7d7b687aae9afb560db86461b
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-05-06 04:02:17 -0400

    feat(ruby-scripting): implement HTTP POST request with JSON body support

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -6,7 +6,14 @@ require "json"
 
 def post_request(url, body_params)
     uri = URI(url)
-    res = Net::HTTP.post_form(uri, body_params)
+
+    req = Net::HTTP::Post.new(uri)
+    req["Content-Type"] = "application/json"
+    req.body = body_params.to_json
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+        http.request(req)
+    end
 
     puts "Response status: #{res.code} #{res.message}"
     puts "Response body:"
