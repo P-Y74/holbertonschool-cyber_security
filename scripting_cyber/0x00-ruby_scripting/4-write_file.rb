ba167f41350e2690182d80c31a19db5a2f4bf278
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-05-06 03:48:02 -0400

    fix(ruby-scripting): merge JSON arrays before writing to file

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -9,5 +9,7 @@ def merge_json_files(file1_path, file2_path)
     arr1 = JSON.parse(file1_content)
     arr2 = JSON.parse(file2_content)
 
-    File.write(file2_path, JSON.pretty_generate(arr2 + arr1))
+    merged = arr2 + arr1
+
+    File.write(file2_path, JSON.pretty_generate(merged))
 end
