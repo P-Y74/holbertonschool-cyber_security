73def9eb342d5080e445d3a0cf92f0e2d0deb66d
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-05-08 03:29:48 -0400

    feat(cli): add tasks header to list output

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -13,6 +13,7 @@ end
 parser.on('-l', '--list', 'List all tasks') do
     if File.exist?("tasks.txt")
         File.readlines("tasks.txt").each_with_index do |task, index|
+            puts "Tasks:"
             puts "#{index + 1}. #{task.strip}"
         end
     else
